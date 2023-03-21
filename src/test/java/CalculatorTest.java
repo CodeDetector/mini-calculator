86ce7d91cdd23cac9111ca80e7b751f0ce695f5d
CodeDetector <dhruvsharma983@gmail.com>
2023-03-21 18:07:53 +0530

    final changes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,35 +11,35 @@ class CalculatorTest {
     private static final double DELTA = 1e-15;
     @Test
     void computeFactorial() {
-        assertEquals( 120, MathCalculator.computeFactorial(5), DELTA);
-        assertEquals( 1, MathCalculator.computeFactorial(0), DELTA);
-        assertEquals( 2, MathCalculator.computeFactorial(2), DELTA);
-        assertEquals( 3628800, MathCalculator.computeFactorial(10), DELTA);
-        assertEquals( 720, MathCalculator.computeFactorial(6), DELTA);
+        assertEquals( 120, calculator.computeFactorial(5), DELTA);
+        assertEquals( 1, calculator.computeFactorial(0), DELTA);
+        assertEquals( 2, calculator.computeFactorial(2), DELTA);
+        assertEquals( 3628800, calculator.computeFactorial(10), DELTA);
+        assertEquals( 720, calculator.computeFactorial(6), DELTA);
 
     }
 
     @Test
     void computeSquareRoot() {
-        assertEquals( 4, MathCalculator.computeSquareRoot(16), DELTA);
-        assertEquals( 1, MathCalculator.computeSquareRoot(1), DELTA);
-        assertEquals( 9, MathCalculator.computeSquareRoot(81), DELTA);
-        assertEquals( 6, MathCalculator.computeSquareRoot(36), DELTA);
+        assertEquals( 4, calculator.computeSquareRoot(16), DELTA);
+        assertEquals( 1, calculator.computeSquareRoot(1), DELTA);
+        assertEquals( 9, calculator.computeSquareRoot(81), DELTA);
+        assertEquals( 6, calculator.computeSquareRoot(36), DELTA);
     }
 
     @Test
     void computePower() {
-        assertEquals(8, MathCalculator.computePower(2, 3), DELTA);
-        assertEquals( 1, MathCalculator.computePower(1, 3), DELTA);
-        assertEquals(81, MathCalculator.computePower(3, 4), DELTA);
-        assertEquals( 64, MathCalculator.computePower(4, 3), DELTA);
-        assertEquals( 64, MathCalculator.computePower(4, 3), DELTA);
+        assertEquals(8, calculator.computePower(2, 3), DELTA);
+        assertEquals( 1, calculator.computePower(1, 3), DELTA);
+        assertEquals(81, calculator.computePower(3, 4), DELTA);
+        assertEquals( 64, calculator.computePower(4, 3), DELTA);
+        assertEquals( 64, calculator.computePower(4, 3), DELTA);
 
     }
 
     @Test
     void computeNaturalLog() {
-        assertEquals( 0, MathCalculator.computeNaturalLog(1), DELTA);
+        assertEquals( 0, calculator.computeNaturalLog(1), DELTA);
 
     }
 
